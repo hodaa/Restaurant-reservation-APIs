abff607be981eeed333a1c7a3e6c79df1f0c71e5
hodaa <hoda@hussin@gmail.com>
2022-02-28 12:25:21 +0200

    enhance query

--- app/Repositories/ReservationRepository.php
+++ app/Repositories/ReservationRepository.php
@@ -5,30 +5,22 @@ use App\Models\Table;
 use Illuminate\Database\Eloquent\Builder;
 use App\Models\Reservation;
 use App\Enums\ReservationStatus;
+use Carbon\Carbon;
 
 class ReservationRepository
 {
 
     public function buildTableAvailableQuery($start_time, $to_time)
     {
+        
         return Table::whereDoesntHave('reservations', function (Builder $q) use ($start_time, $to_time) {
             $q->where(function($query)  use ($start_time, $to_time){
-                $query->where('reservations.from_time', '<=', $start_time);
-                $query->where('reservations.from_time', '<=', $to_time);
-                $query->where('reservations.to_time', '>=', $start_time);
-                $query->where('reservations.to_time', '>=', $to_time);
-                $query->where('reservations.status', '!=', ReservationStatus::CHECKOUT);
-             })->orWhere(function($query)  use ($start_time, $to_time){
-                $query->where('reservations.from_time', '>=', $start_time);
-                $query->where('reservations.from_time', '<', $to_time);
-                $query->where('reservations.to_time', '>=', $start_time);
-                $query->where('reservations.to_time', '>=', $to_time);
-                $query->where('reservations.status', '!=', ReservationStatus::CHECKOUT);
-             })->orWhere(function($query)  use ($start_time, $to_time){
-                $query->where('reservations.from_time', '>=', $start_time);
-                $query->where('reservations.from_time', '<', $to_time);
-                $query->where('reservations.to_time', '>=', $start_time);
-                $query->where('reservations.to_time', '<', $to_time);
+                
+                $start_time_extra = Carbon::parse($start_time)->addMinute(1)->toTimeString();
+                $to_time_minus = Carbon::parse($to_time)->subMinute(1)->toTimeString();
+
+                $query->whereBetween('reservations.from_time', [$start_time,$to_time_minus]);
+                $query->orWhereBetween('reservations.to_time', [$start_time_extra,$to_time]);
                 $query->where('reservations.status', '!=', ReservationStatus::CHECKOUT);
              });
 
